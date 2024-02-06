ddb3e7c3ae7673ee6cb6aaf545c5290389de05f3
ahmadihsanullah <ahma21327ti@student.nurulfikri.ac.id>
2024-02-06 10:34:48 +0700

    mencoba mengaktifkan thread daemon dijava(bukan unit test)

--- src/main/java/programmer/zaman/now/thread/MainApplication.java
+++ src/main/java/programmer/zaman/now/thread/MainApplication.java
@@ -2,7 +2,23 @@ package programmer.zaman.now.thread;
 
 public class MainApplication {
     public static void main(String[] args) {
-        var name = Thread.currentThread().getName();
-        System.out.println(name);
+        // var name = Thread.currentThread().getName();
+        // System.out.println(name);
+
+        Runnable runnable = () -> {
+            try {
+                Thread.sleep(2000);
+                System.out.println("Hello from thread : " + Thread.currentThread().getName());
+            } catch (InterruptedException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        };
+      
+          var thread = new Thread(runnable);
+        //   thread.setDaemon(true); Thread Daemon membuat thread tidak menunggu program selesai
+          thread.start();
+      
+          System.out.println("Program Selesai");
     }
 }
